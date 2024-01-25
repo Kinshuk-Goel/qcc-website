1642f30ee48c3e1fd742e9af050ab6c175b2b5e1
Kinshuk Goel <67846964+Kinshuk-Goel@users.noreply.github.com>
2024-01-25 13:56:11 -0500

    Removed Stats, didnt need

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ const Home: React.FC = () => {
         </div>
         <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
-            <Stats />
+            {/* <Stats /> */}
             <Business />
             <Billing />
             <CardDeal />
